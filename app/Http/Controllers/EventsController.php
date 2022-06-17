b8cef1a5f7cb6d2f9371d3f3d007799667fc64ac
Anush <anush@hubilo.com>
2022-06-17 16:23:40 +0530

    test cases fixed

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -3,16 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Models\Event;
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-use Illuminate\Foundation\Bus\DispatchesJobs;
-use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
-use Illuminate\Support\Facades\Date;
+use Illuminate\Support\Carbon;
+use DB;
 
 class EventsController extends BaseController
 {
     public function getWarmupEvents() {
-        return Event::all();
+        return Event::select('name')->get();
     }
 
     /*
@@ -101,7 +99,7 @@ class EventsController extends BaseController
      */
 
     public function getEventsWithWorkshops() {
-        throw new \Exception('implement in coding task 1');
+        return Event::with('workshops')->get();
     }
 
 
@@ -180,6 +178,6 @@ class EventsController extends BaseController
      */
 
     public function getFutureEventsWithWorkshops() {
-        throw new \Exception('implement in coding task 2');
+        return Event::with('workshops')->whereRelation('workshops', 'start' , '>', Carbon::now())->get();
     }
 }
--- app/Models/Event.php
+++ app/Models/Event.php
@@ -7,4 +7,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Event extends Model
 {
+    protected $connection = 'mysql';
+    protected $table = 'events';
+    protected $primaryKey = 'id';
+    public $incrementing = true;
+    public $timestamps = true;
+    const CREATED_AT = 'created_at';
+    const UPDATED_AT = 'updated_at';
+
+    public function workshops()
+    {
+        return $this->hasMany(Workshop::class);
+    }
 }
